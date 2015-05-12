873aca982a3739b38a7c07950df15a283c51f3e1
pmontoto <pmgberm@gmail.com>
2015-05-12 17:34:34 +0200

    Version after tried json

--- twitter_analysis_galiza/index.js
+++ twitter_analysis_galiza/index.js
@@ -11,25 +11,43 @@ twitter.getRequestToken(function(error, requestToken, requestTokenSecret, result
   if (error) {
     console.log("Error getting OAuth request token : " + JSON.stringify(error));
   } else {
-    token = {
+    token['request'] = {
       'token' : requestToken,
       'secret' : requestTokenSecret
     };
-    //store token and tokenSecret somewhere, you'll need them later; redirect user 
   }
-  console.log(JSON.stringify(token));
+  
+  var rl = require('readline').createInterface(process.stdin, process.stdout);
+  rl.question("Auth token: ", function(answer) {
+    token['token'] = answer.trim();
+
+    rl.question("Auth secret: ", function(answer) {
+      token['secret'] = answer.trim();
+      console.log(JSON.stringify(token));
+
+      rl.question("Query: ", function(answer) {
+        twitter.search(
+          {
+            'q' : answer.trim(),
+            'result_type' : 'recent',
+            'count' : 100
+          },
+          token.token,
+          token.secret,
+          function (error, data, response) {
+           for (var tweet_n in data.statuses) {
+             var tweet = data.statuses[tweet_n];
+             console.log("Tweet key " + tweet_n + " : ");
+
+             console.log("> " + tweet.user.name + " @ " + tweet.user.location);
+             console.log(">>>> geo " + JSON.stringify(tweet.geo));
+             console.log(">>>> coo " + JSON.stringify(tweet.coordinates));
+           
+             console.log("");
+           } 
+        });
+      });
+    });
+  });
 });
-
-
-twitter.search("tweets", 
-  {
-    'q' : "galiciabilingue"
-  },
-  token.token,
-  token.secret,
-  function (error, data, response) {
-    console.log("Error :" + JSON.stringify(error));
-    console.log("Data : " + JSON.stringify(data));
-  }
-);
 
